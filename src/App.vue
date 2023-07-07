cdfe0b8dca742d2f2b12079dcb4b042fa3a390d4
iputuartika <iputuartika@gmail.com>
2023-07-07 17:08:42 +0800

    Perbaikan view untuk mobile

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,24 @@
 import { RouterLink, RouterView } from 'vue-router'
 import NavbarComponent from './components/NavbarComponent.vue'
 import Sidebar from '@/components/sidebar/Sidebar.vue'
-import PageWrapper from './components/PageWrapper.vue'
 import { sidebarState } from '@/composables'
+import Breadcrumb from '@/components/Breadcrumb.vue'
+import Footer from '@/components/Footer.vue'
 </script>
 
 <template>
   <NavbarComponent />
   <Sidebar />
-  <main :class="sidebarState.isOpen ? 'pl-64' : ''" class="h-screen dark:bg-gray-700 pt-16">
-    <RouterView />
+  <main
+    :class="sidebarState.isOpen ? 'md:pl-70' : 'md:pl-20'"
+    class="px-0 md:px-8 min-h-screen dark:bg-gray-900 py-16 pl-"
+  >
+    <div class="pb-2">
+      <Breadcrumb />
+    </div>
+    <div class="pb-2">
+      <RouterView />
+    </div>
   </main>
+  <Footer />
 </template>
